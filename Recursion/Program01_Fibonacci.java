b3a4d840ee96ff8a16788a453a557072375c09a1
chakrabortyDebasish <debasish2392000@gmail.com>
2024-12-30 21:30:10 +0530

    fibonaci program

--- Recursion/Program01_Fibonacci.java
+++ Recursion/Program01_Fibonacci.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Program01_Fibonacci {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("@Author : Debasish Chakraborty");
+        System.out.println("Enter the place holder of the number to be printed for the series : ");
+
+        int placeholder = sc.nextInt();
+
+        int result = Fibonacci(placeholder);
+
+        System.out.println("The number present at position " + placeholder + " is : " + result);
+
+        sc.close();
+    }
+
+    private static int Fibonacci(int placeholder) {
+
+        if (placeholder == 0)
+            return 0;
+        else if (placeholder == 1)
+            return 1;
+
+        int output = Fibonacci(placeholder - 1) + Fibonacci(placeholder - 2);
+
+        return output;
+
     }
 }
